03b5987361e5050cc20dd28919b33033ec175933
Daniel Melo <daniel-araujo.melo@serpro.gov.br>
2015-09-08 14:25:30 -0300

    Added active tab highlight; Added GitHub ribbon

---
+++ src/test/controllers/MenuControllerTest.ts
@@ -0,0 +1,36 @@
+module SwapApp.Tests.Controllers {
+
+  import MenuController = SwapApp.Controllers.MenuController;
+
+  QUnit.module('MenuController');
+
+  QUnit.test('Test isActiveTab with same location', (assert : QUnitAssert) => {
+
+    var mock$location = <ng.ILocationService> {
+      path () {
+        return '/some/url';
+      }
+    }
+
+    var ctrl = new MenuController(mock$location);
+    var returnValue = ctrl.isActiveTab('/some/url');
+    assert.ok(returnValue, 'isActiveTab returns true for same URL');
+
+  });
+
+  QUnit.test('Test isActiveTab with similar location', (assert : QUnitAssert) => {
+
+    var mock$location = <ng.ILocationService> {
+      path () {
+        return '/some/url';
+      }
+    }
+
+    var ctrl = new MenuController(mock$location);
+    var returnValue = ctrl.isActiveTab('/some/url/something');
+    assert.ok(!returnValue, 'isActiveTab returns false for similar URL');
+
+  });
+
+
+}
--- src/ts/controllers/MenuController.ts
+++ src/ts/controllers/MenuController.ts
@@ -1,9 +1,14 @@
 module SwapApp.Controllers {
 
   @at.controller(SwapApp.mainModule, 'menuController')
+  @at.inject('$location')
   export class MenuController {
 
-    constructor() {
+    constructor(private $location : ng.ILocationService) {
+    }
+
+    isActiveTab(url : string) : boolean {
+      return url === this.$location.path();
     }
 
   }
