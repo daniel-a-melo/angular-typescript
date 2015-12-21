b2c5cb25a7a36dfcfc084ea5e616993c90218ea0
Daniel Melo <daniel-araujo.melo@serpro.gov.br>
2015-12-21 14:21:27 -0300

    Webpack tweak to include web fonts as assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,33 @@ var path = require('path');
 
 var webpack = require('webpack');
 var srcDir = path.resolve(__dirname, 'src/ts');
-var outputDir = path.resolve(__dirname, 'app');
+var outputDir = path.resolve(__dirname, 'assets');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: "./src/ts/AppInit.ts",
     output: {
-        path: __dirname,
-        filename: "bundle.js"
+        path: outputDir,
+        filename: "bundle.js",
+        publicPath : '/assets/'
     },
     resolve: {
       // Add `.ts` and `.tsx` as a resolvable extension.
       extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
     },
     devServer : {
-      contentBase : outputDir
+      contentBase : 'app'
     },
     module: {
         loaders: [
             { test: /\.css$/, loader: 'style!css' },
             { test: /\.tsx?$/, loader: 'ts-loader' },
-            { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.woff2$|\.eot$|\.ttf$|\.wav$|\.mp3$/, loader: require.resolve("file-loader") + "?name=../[path][name].[ext]"}
-            // { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
-            // { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" },
-            // { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
-            // { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
-            // { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
+            //{ test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.woff2$|\.eot$|\.ttf$|\.wav$|\.mp3$/, loader: require.resolve("file-loader") + "?name=../[path][name].[ext]"}
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
         ]
     },
     plugins : [
